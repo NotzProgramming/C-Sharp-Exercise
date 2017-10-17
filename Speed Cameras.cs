09964f741507ea84ab60f2e0d881f41a1fe07657
Notz <ggwplol5513@gmail.com>
2017-10-17 16:32:50 +0800

    Update Speed Cameras.cs

--- Speed Cameras.cs
+++ Speed Cameras.cs
@@ -6,6 +6,17 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            /* Your job is to write a program for a speed camera. For simplicity,
+             * ignore the details such as camera, sensors, etc and focus purely on 
+             * the logic. Write a program that asks the user to enter the speed limit.
+             * Once set, the program asks for the speed of a car. If the user enters 
+             * a value less than the speed limit, program should display Ok on the 
+             * console. If the value is above the speed limit, the program should 
+             * calculate the number of demerit points. For every 5km/hr above the 
+             * speed limit, 1 demerit points should be incurred and displayed on 
+             * the console. If the number of demerit points is above 12, the program
+             * should display License Suspended.*/
+
             Console.WriteLine("Enter speed limited.");
             var input1 = Console.ReadLine();
             Console.WriteLine("Enter the speed of your car.");
@@ -22,9 +33,9 @@ namespace ConsoleApp1
                 Console.WriteLine("Your demerit points is " + number2);
                 if (number2 >= 12)
                     Console.WriteLine("License Suspended.");
-            }   
+            }
         }
 
-        
+
     }
 }
